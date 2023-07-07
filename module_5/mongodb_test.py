e9abd1462264f99378b12747339120f9df14f833
programplayer1 <plorenz3@hotmail.com>
2023-07-07 18:48:05 -0500

    first commit

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.bcg6ygj.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+
+db = client.pytech
+
+print(db.list_collection_names())
+
