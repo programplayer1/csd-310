b3184ba63c6bb89ed8a12593a53ffa3d8a96b2b1
programplayer1 <plorenz3@hotmail.com>
2023-07-12 18:00:01 -0500

    commit

--- module_6/pytech_delete.py
+++ module_6/pytech_delete.py
@@ -1,11 +1,9 @@
 """
 Author:  Paul Lorenz III
 Date: July 12, 2023
-Description: Inserting and deleting the data from collection using pymongo 
+Description: Inserting and deleting the data from collection using pymongo.
 
 """
-
-
 
 ##Used the import statement to import MongoClient from MongoDB Atlas and linked the url to establish the MongoDB Client connection.
 from pymongo import MongoClient
@@ -29,24 +27,27 @@ for x in All_data:
     print("Last_Name :" , x['last_name'])
     print()
 
+##Assigned a variable to create a new student document in the "students" collection of the pytech database.
 insert_student_document1 = {
     "student_id": 1010,
     "first_name": "Russel",
     "last_name" : "Granger"
 }
 
+##Executed the insert_one method to insert a new student document.
 student_result = mydata.insert_one(insert_student_document1)
 
+##Executed the output statement to inform the user of the type of action that will get performed by the program.
 print("-- INSERT STATEMENTS --")
 
 print("Inserted student record Russel Granger into the students collection with document id",  student_result.inserted_id)
 
-print("f\n-- DISPLAYING STUDENT TEST DOC --")
+print("\n-- DISPLAYING STUDENT TEST DOC --")
 
 ##Executed the find_one method to find one student document that resides in the collection of the database.
 x_data = mydata.find_one({"student_id":1010})
 
-##Executed an if statement to retrieve the information that goes along with one student document that is in the collection of the database.
+##Assigned variables to retrieve the information that goes along with one student document that is in the collection of the database.
 student_id = x_data["student_id"]
 first_name = x_data["first_name"]
 last_name = x_data["last_name"]
@@ -56,12 +57,13 @@ print("Student ID:", student_id)
 print("First Name:", first_name)
 print("Last Name:", last_name)
 
-
+##Executed the delete_one method to delete a student document.
 DeleteQuery = {"student_id":1010}
 
 mydata.delete_one(DeleteQuery)
 
-
+##Executed the output statement to inform the user of the type of action that will get performed by the program.
+print("\n-- DISPLAYING STUDENT DOCUMENTS FROM find() Query --")
 
 ##Executed the find() method to display all student documents in the collection of the database.
 All_data = mydata.find({},{"student_id":1, "first_name":1,"last_name":1})
